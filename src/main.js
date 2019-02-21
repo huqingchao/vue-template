2b61a747721a06cae0d3c8b75053eaf073d92640
huqingchao <1026489907@qq.com>
2019-02-21 19:20:57 +0800

    项目第一天

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
 // 入口文件
 import Vue from 'vue'
 
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
 import './lib/mui/css/mui.min.css'
 
-import { Header } from 'mint-ui'
+import './lib/mui/css/icons-extra.css'
 
-Vue.component(Header.name,Header)
+import { Header, Swipe, SwipeItem } from 'mint-ui'
+Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+
+import router from './router.js'
 
 import app from './App.vue'
 
 var vm = new Vue({
 	el: '#app',
-	render: c => c(app)
+	render: c => c(app),
+	router
 })
